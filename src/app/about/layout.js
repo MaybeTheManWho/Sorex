74a901e3827ffa6c2ab36d4db1925baa7b47678f
MaybeTheManWho <samaiscool7@gmail.com>
2025-04-02 12:32:00 +0300

    Delete src directory

--- src/app/about/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-    title: 'About | Your Portfolio',
-    description: 'Learn more about my background, skills, and experience',
-  };
-  
-  export default function AboutLayout({ children }) {
-    return children;
-  }
--- src/app/blog/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-    title: 'Blog | Your Portfolio',
-    description: 'Read my latest thoughts, tutorials, and insights',
-  };
-  
-  export default function BlogLayout({ children }) {
-    return children;
-  }
